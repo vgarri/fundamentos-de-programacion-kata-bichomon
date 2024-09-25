32114e7378902a310a89ced338924de8c92f4466
Victor Garritano <victor@getupgraded.com>
2024-09-25 20:40:57 +0200

    script terminado sin premium

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-console.log(document.title);
+
+
+//1
+document.querySelector("#gen-1").innerHTML = "Generasión 1 Pokimon";
+//2
+let tarjetitas = document.querySelectorAll(".infocard-list");
+tarjetitas[0].style.backgroundColor ='red';
+//3
+console.log(document.URL);
+//4
+console.log(window.location);
+//5
+console.log(document.querySelectorAll("img"));
+//6
+//"https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
+let sources = document.querySelectorAll("img");
+    sources.forEach((imagen)=> {
+        imagen.src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+    });
+
+
